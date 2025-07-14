f645337acf66e50ccf901358f8fc40834faf9a61
JunelAmelon <junelamelon92@gmail.com>
2025-07-14 17:15:53 +0100

    update

--- components/PDFGenerator.tsx
+++ components/PDFGenerator.tsx
@@ -36,7 +36,7 @@ export const PDFGenerator: React.FC<{ className?: string; iconOnly?: boolean }> 
         const formData = new FormData();
         formData.append('file', pdfBlob, fileName);
         formData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!);
-        formData.append('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!);
+       
         const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/auto/upload`, {
           method: 'POST',
           body: formData,
