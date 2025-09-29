939f2c082210076f03706a2479467f55bfa434cf
AdnaneAD1 <sidiamadouadnane4@gmail.com>
2025-09-29 09:13:08 +0200

    maintenance 0.2

--- config/maintenance.ts
+++ config/maintenance.ts
@@ -1,7 +1,7 @@
 // Configuration de la maintenance
 export const MAINTENANCE_CONFIG = {
   // Mettre à true pour activer le mode maintenance
-  enabled: false,
+  enabled: true,
   
   // Message personnalisé (optionnel)
   message: "Notre plateforme est actuellement en cours de maintenance pour vous apporter de nouvelles fonctionnalités passionnantes ! Nous serons de retour dans quelques heures avec de nouvelles améliorations.",
